4e4811709f3d2d65fadec9dc540913b0f84884df
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-08 23:06:31 -0400

    module files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeRoutingModule } from './home/home-routing.module';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HomeRoutingModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }

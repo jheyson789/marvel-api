5b1252bc3ec3ac5441e3dc4ceb8ac6fdb2660fc0
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-15 22:00:26 -0400

    characters module

--- src/app/home/pages/characters/services/characters.service.ts
+++ src/app/home/pages/characters/services/characters.service.ts
@@ -1,16 +1,15 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpParams} from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CharactersService {
 
-  constructor(private _http:HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
   getCharacters(filter: {} = {}) {
     const params = new HttpParams().set('filter', JSON.stringify(filter))
     return this._http.get('characters', {params})
   }
-
 }

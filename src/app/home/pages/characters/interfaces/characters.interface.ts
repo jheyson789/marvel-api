ef85c9da2827ea09881eff2f73b27a1e7419c792
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-15 22:20:33 -0400

    interfaces to characters

---
+++ src/app/home/pages/characters/interfaces/characters.interface.ts
@@ -0,0 +1,16 @@
+export interface IThumbnail {
+  path: string
+  extension: string
+}
+
+// TODO agregar los types que faltan
+// comics events stories url's
+export interface ICharacters {
+  id: number
+  name: string
+  description: string
+  modified: string
+  thumbnail: IThumbnail
+  resourceURI: string
+}
+
--- src/app/home/pages/characters/services/characters.service.ts
+++ src/app/home/pages/characters/services/characters.service.ts
@@ -1,6 +1,15 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { ICharacters } from '../interfaces/characters.interface';
 
+// TODO cambiar la posibilidad de extends o implements o inject type
+export type dataCharacter = {
+  count: number
+  limit: number
+  offset: number
+  results: [ICharacters]
+  total: number
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -10,6 +19,6 @@ export class CharactersService {
 
   getCharacters(filter: {} = {}) {
     const params = new HttpParams().set('filter', JSON.stringify(filter))
-    return this._http.get('characters', {params})
+    return this._http.get<{data:dataCharacter}>('characters', {params})
   }
 }

5869133d3d8a854b63b8c9970b9bb7d887cba6f0
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-16 16:21:49 -0400

    filter with pagination

--- src/app/home/pages/characters/services/characters.service.ts
+++ src/app/home/pages/characters/services/characters.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { CharacterDataWrapper } from '../interfaces/characters.interface';
+import {
+  CharacterDataWrapper,
+  filterCharacter,
+} from '../interfaces/characters.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -8,8 +11,8 @@ import { CharacterDataWrapper } from '../interfaces/characters.interface';
 export class CharactersService {
   constructor(private _http: HttpClient) {}
 
-  getCharacters(filter: {} = {}) {
-    const params = new HttpParams().set('filter', JSON.stringify(filter));
+  getCharacters({ offset, limit }: filterCharacter) {
+    const params = new HttpParams().set('offset', offset).set('limit', limit);
     return this._http.get<CharacterDataWrapper>('characters', { params });
   }
 }

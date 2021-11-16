af0b23d6c81633fbbf23ff81ef298f6169b62f2d
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-16 16:21:33 -0400

    pagination controls

---
+++ src/app/shared/pagination/pagination.component.ts
@@ -0,0 +1,25 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { filterCharacter } from 'src/app/home/pages/characters/interfaces/characters.interface';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.css'],
+})
+export class PaginationComponent implements OnInit {
+  @Input('page') public page: number = 0;
+
+  @Output() filter = new EventEmitter<filterCharacter>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  emitPagination(page: number) {
+    const filter: filterCharacter = {
+      offset: page <= 0 ? 0 : page,
+      limit: 21,
+    };
+    this.filter.emit(filter);
+  }
+}

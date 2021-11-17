f1d4ce5d5a22fb25c2b2e9ac724046672f3be24a
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-17 12:32:45 -0400

    profile the character

--- src/app/home/pages/character/character.component.ts
+++ src/app/home/pages/character/character.component.ts
@@ -1,15 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CharacterService } from './services/character.service';
+import { Character } from '../interfaces/characters.interface';
 
 @Component({
   selector: 'app-character',
   templateUrl: './character.component.html',
-  styleUrls: ['./character.component.css']
+  styleUrls: ['./character.component.css'],
 })
 export class CharacterComponent implements OnInit {
+  private characterId: number;
+  public character: Character = {
+    id: 0,
+    name: '',
+    description: '',
+    modified: new Date(),
+    resourceURI: '',
+    urls: [],
+    thumbnail: {
+      extension: '',
+      path: '',
+    },
+  };
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _route: ActivatedRoute,
+    private _characterService: CharacterService
+  ) {
+    this.characterId = Number(this._route.snapshot.paramMap.get('id')!);
   }
 
+  ngOnInit(): void {
+    this.findCharacter(this.characterId);
+  }
+
+  findCharacter(id: number) {
+    this._characterService.getCharacter(id).subscribe(
+      (resp) => {
+        this.character = resp.data.results[0];
+      },
+      (err) => console.log(err)
+    );
+  }
 }

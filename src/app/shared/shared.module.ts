6c6e336c2887079e66fdb53fb5dfc2b4d30ae1c3
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-17 16:10:12 -0400

    features component into character

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,21 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { RouterModule } from '@angular/router';
 import { PaginationComponent } from './pagination/pagination.component';
 import { SpinnerComponent } from './spinner/spinner.component';
+import { FeaturesComponent } from './features/features.component';
 
 @NgModule({
-  declarations: [HeaderComponent, PaginationComponent, SpinnerComponent],
+  declarations: [
+    HeaderComponent,
+    PaginationComponent,
+    SpinnerComponent,
+    FeaturesComponent,
+  ],
   imports: [CommonModule, FontAwesomeModule, RouterModule],
-  exports: [HeaderComponent, PaginationComponent, SpinnerComponent],
+  exports: [
+    HeaderComponent,
+    PaginationComponent,
+    SpinnerComponent,
+    FeaturesComponent,
+  ],
 })
 export class SharedModule {}

aa86ed69b98f7d4a6eef215b0d153a93ca931ef3
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-18 11:51:26 -0400

    card component

--- src/app/home/landing/main/main.module.ts
+++ src/app/home/landing/main/main.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main-routing.module';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [MainComponent],
-  imports: [CommonModule, MainRoutingModule],
+  imports: [CommonModule, ComponentsModule, MainRoutingModule],
 })
 export class MainModule {}

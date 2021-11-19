c6ad9902c4f8ea09636ff624a33132600eb41960
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-18 21:35:54 -0400

    remove files

--- README.md
+++
@@ -1,3 +0,0 @@
-# MarvelApi
-
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.2.11.
--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  backend: 'https://gateway.marvel.com:443/v1/public/',
-  apiKey: '6dddab4e31b504abad1633fd4a1f1129',
-  hash: '38abdcf3f061f5f0aa9701d697b11642'
-};

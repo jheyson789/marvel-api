293d5260500fee6320dae9fc4d6b777654fb4cec
jheyson zurita soria <jheysoria20@gmai.com>
2021-11-18 20:51:37 -0400

    provide test module

--- src/app/home/pages/characters/characters.component.spec.ts
+++ src/app/home/pages/characters/characters.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CharactersComponent } from './characters.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('CharactersComponent', () => {
   let component: CharactersComponent;
@@ -9,6 +10,7 @@ describe('CharactersComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CharactersComponent],
+      imports: [HttpClientTestingModule],
     }).compileComponents();
   });
 
